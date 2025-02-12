e0972518fafd3f297e80392cc5ff7a42b33837c2
Sahara Sipple <sahara36@usf.edu>
2025-02-11 19:39:31 -0500

    Task 2 - Product Price After Discount

--- cc_8.js
+++ cc_8.js
@@ -7,3 +7,13 @@ function calculateSalary(baseSalary, bonus, taxRate) {  // Declare function and 
 
 calculateSalary(5000,500,0.1); // Expected Output: "Net Salary: $5000.00"
 calculateSalary(7000,1000,0.15); // Expected Output: "Net Salary: $6950.00"
+
+// Task 2: Function Expression //
+
+function calculateDiscount(price, discountRate) { // Declare function and variables
+    let finalPrice = price - (price * discountRate); // Defines finalPrice and the formula
+    console.log(`Final Price: $${finalPrice.toFixed(2)}`); // Console log the finalPrice to 2 decimal places
+}
+
+calculateDiscount(100, 0.2); // Expected Output: "Final Price: $80.00"
+calculateDiscount(250, 0.15); // Expected Output: "Final Price: $212.50"
